13c03b0f1610f803026f9040472026c0f05a1e7d
renato <renato@nitro5c.home>
2023-08-10 10:48:43 -0300

    change from rtmp to hls

--- live.py
+++ live.py
@@ -35,10 +35,12 @@ def streamIpcam (ipcamId, host, port, rtsp, time_limit):
         cam_on = checkHostPort(host, port)
         if (cam_on):
             streamUrl = getStreamUrl(ipcamId)
-            cmd = "ffmpeg -t " + time_limit + " -nostdin -timeout 5000000 -i " + rtsp + " -vcodec copy -acodec copy -f flv " + streamUrl + " > /dev/null 2>&1 "
+            parts = streamUrl.rsplit('/', 1)
+            streamUrlHls = parts[0] + 'stream.m3u8'
+            cmd = "ffmpeg -t " + time_limit + " -i " + rtsp + " -c:v copy -f hls -method PUT -hls_time 1 -hls_playlist_type event -http_persistent 1 '" + streamUrlHls + "' > /dev/null 2>&1"
             p = Popen(cmd, shell=True)
             print(f'started stream from {host}:{port} {rtsp}')
-            print(f'stream url "{streamUrl}"')
+            print(f'stream url "{streamUrlHls}"')
         else:
             print(f'Ipcam not up, sleeping 10 seconds...')
             p = Popen("sleep 10", shell = True)
