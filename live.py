afcfaffcf1cf27d9e12807512cef457ee7ed76a0
renato <renato@nitro5c.home>
2023-08-15 10:34:21 -0300

    added timeout parameter to avoid rtsp stall

--- live.py
+++ live.py
@@ -37,7 +37,7 @@ def streamIpcam (ipcamId, host, port, rtsp, time_limit):
             streamUrl = getStreamUrl(ipcamId)
             parts = streamUrl.rsplit('/', 1)
             streamUrlHls = parts[0] + 'stream.m3u8'
-            cmd = "ffmpeg -t " + time_limit + " -i " + rtsp + " -c:v copy -f hls -method PUT -hls_time 1 -hls_playlist_type event -http_persistent 1 '" + streamUrlHls + "' > /dev/null 2>&1"
+            cmd = "ffmpeg -timeout 5 -t " + time_limit + " -i " + rtsp + " -c:v copy -f hls -method PUT -hls_time 1 -hls_playlist_type event -http_persistent 1 '" + streamUrlHls + "' > /dev/null 2>&1"
             p = Popen(cmd, shell=True)
             print(f'started stream from {host}:{port} {rtsp}')
             print(f'stream url "{streamUrlHls}"')
